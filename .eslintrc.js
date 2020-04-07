8710714fd7a63287602b299aa26e548b7b462229
Jimmba <gimvit@gmail.com>
2020-04-07 03:07:38 +0300

    fix: crlf

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    //"extends": "eslint:recommended",
     "extends": "airbnb-base",
     "globals": {
         "Atomics": "readonly",
